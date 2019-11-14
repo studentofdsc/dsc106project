14b93e3bbb53c2afd76f46df29c2a849216153a8
Eric Wang <elwang@ucsd.edu>
2019-11-13 22:15:38 -0800

    first sub

--- main.js
+++ main.js
@@ -1,4 +1,3 @@
-
 ['mousemove', 'touchmove', 'touchstart'].forEach(function (eventType) {
     document.getElementById('container').addEventListener(
         eventType,
@@ -14,7 +13,6 @@
                 event = chart.pointer.normalize(e);
                 // Get the hovered point
                 point = chart.series[0].searchPoint(event, true);
-
                 if (point) {
                     point.highlight(e);
                 }
@@ -64,6 +62,10 @@ function syncExtremes(e) {
     }
 }
 
+
+var global_timestamp_to_val_dict = {}
+
+
 function parse_json(jsonData) {
     var energyData = jsonData.filter(function(elm) {
         return elm['type'] === 'power' && elm['id'] !== 'Springfield.fuel_tech.rooftop_solar.power';
@@ -74,19 +76,28 @@ function parse_json(jsonData) {
         };
     });
 
-    var startTime = Date(jsonData[0]['history']['start']*1000)
+    var startTime = jsonData[0]['history']['start']*1000
 
     for (i = 0; i < energyData.length;i++){
         var sampled_value_lst = []
         var timestamp = jsonData[0]['history']['start']*1000
+        var energy_id = jsonData[i]['id'].substring(22)
         for (j = 0; j < energyData[i]['values'].length;j+=6){
-            var sampled_value_with_timestamp = [timestamp,energyData[i]['values'][j]]
+            var sampled_value = energyData[i]['values'][j]
+            var sampled_value_with_timestamp = [timestamp,sampled_value]
+            if (timestamp in global_timestamp_to_val_dict){
+                global_timestamp_to_val_dict[timestamp].push([energy_id,sampled_value])
+            }
+            else{
+                global_timestamp_to_val_dict[timestamp] = [[energy_id,sampled_value]]
+            }
             sampled_value_lst.push(sampled_value_with_timestamp)
             timestamp += 1000*60*30
 
         }
         energyData[i]['values'] = sampled_value_lst
     }
+    console.debug(global_timestamp_to_val_dict)
 
 
     area_chart = Highcharts.chart('container', { 
@@ -94,17 +105,12 @@ function parse_json(jsonData) {
             type: 'area'
         },
         title:{
-            text:'Energy Springfield'
+            text:'Energy Springfield',
+            align:'left'
         },
         xAxis:{
             crosshair:true,
             type:'datetime'
-        },
-        legend:{
-            align:'right',
-            verticalAlign:'bottom',
-            layout:'vertical',
-            width:'50%'
         },
         plotOptions:{
             area:{
@@ -114,6 +120,16 @@ function parse_json(jsonData) {
                         this.update({
                             zIndex:1
                         })
+                    }
+                }
+            },
+            series:{
+                states:{
+                    hover:{
+                        enabled:false
+                    },
+                    inactive:{
+                        opacity:1
                     }
                 }
             }
@@ -137,57 +153,63 @@ function parse_json(jsonData) {
         series:[{
             name:energyData[1]['text'],
             data:energyData[1]['values'],
+            color:'#C74523',
             tooltip:{
+                valuePrefix:'Distillate ',
                 valueSuffix:' '+jsonData[1]['units']
             }
         },{
             name:energyData[2]['text'],
             data:energyData[2]['values'],
+            color:'#FDB462',
             tooltip:{
+                valuePrefix:'Gas (CCGT) ',
                 valueSuffix:' '+jsonData[2]['units']
             }
         },{
             name:energyData[3]['text'],
             data:energyData[3]['values'],
+            color:'#4582B4',
             tooltip:{
+                valuePrefix:'Hydro ',
                 valueSuffix:' '+jsonData[3]['units']
             }
         },{
             name:energyData[4]['text'],
             data:energyData[4]['values'],
+            color:'#88AFD0',
             tooltip:{
+                valuePrefix:'Pumps ',
                 valueSuffix:' '+jsonData[4]['units']
             }
         },{
             name:energyData[5]['text'],
             data:energyData[5]['values'],
+            color:'#437607',
             tooltip:{
+                valuePrefix:'Wind ',
                 valueSuffix:' '+jsonData[5]['units']
             }
         },{
             name:energyData[6]['text'],
             data:energyData[6]['values'],
+            color:'#977AB1',
             tooltip:{
+                valuePrefix:' Exports ',
                 valueSuffix:' '+jsonData[6]['units']
             }
         },{
             name:energyData[0]['text'],
             data:energyData[0]['values'],
+            color:'#121212',
             tooltip:{
+                valuePrefix:'Black Coal ',
                 valueSuffix:' '+jsonData[0]['units']
             }
         }]
 	});
 
-    // Highcharts.chart('container_donut',{
-    //     chart:{
-    //         type:'variablepie'
-    //     },
-    //     series:{
-    //         innerSize:'50%',
-    //         data:energyData[0]['values']
-    //     }
-    // })
+
 
     var priceData = jsonData.filter(function(elm) {
         return elm['type'] === 'price';
@@ -209,7 +231,6 @@ function parse_json(jsonData) {
         }
         priceData[i]['values'] = sampled_value_lst
     }
-    console.debug(priceData[0]['values'].length)
 
    price_line_chart = Highcharts.chart('container2',{
         xAxis:{
@@ -302,7 +323,51 @@ function parse_json(jsonData) {
                 valueSuffix:' '+'°F'
             }
         }]
-    })    
+    })
+
+    var point_data_lst = global_timestamp_to_val_dict[startTime]
+
+    var pie_chart = Highcharts.chart('container_pie',{
+            chart:{
+                type:'pie'
+            },
+            title:{
+                text:""
+            },
+            series:[{
+                data:[{
+                    name:point_data_lst[0][0],
+                    y:point_data_lst[0][1],
+                    color:'#121212'
+                },{
+                    name:point_data_lst[1][0],
+                    y:point_data_lst[1][1],
+                    color:'#C74523'
+                },{
+                    name:point_data_lst[2][0],
+                    y:point_data_lst[2][1],
+                    color:'#FDB462'
+                },{
+                    name:point_data_lst[3][0],
+                    y:point_data_lst[3][1],
+                    color:'#4582B4'
+                },{
+                    name:point_data_lst[4][0],
+                    y:point_data_lst[4][1],
+                    color:'#88AFD0'
+                },{
+                    name:point_data_lst[5][0],
+                    y:point_data_lst[5][1],
+                    color:'#437607'
+                },{
+                    name:point_data_lst[6][0],
+                    y:point_data_lst[6][1],
+                    color:'#977AB1'
+                }]
+            }]
+        })
+
+
 
 }
 function fetchJSONFile(filePath, callbackFunc) {
